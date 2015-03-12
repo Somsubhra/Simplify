fb49b9b30c1f0dec2fa3fa6cb985149aa0ea303f
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2015-03-12 14:33:48 +0530

    Split Lexical Simplifier for corpus as well a webapp

--- src/lexus/lexical_simplifier.py
+++ src/lexus/lexical_simplifier.py
@@ -38,12 +38,12 @@ class LexicalSimplifier:
                 in_file = path.join(dir_path, file_name)
                 out_file = path.join(self.out_dir, file_name + '_' + dir_path.replace('/', '_') + '.txt')
 
-                self.simplify(in_file, out_file)
+                self.simplify_file(in_file, out_file)
 
         Logger.log_success('Lexical Simplifier finished successfully')
 
     # Simplify a given file
-    def simplify(self, in_file, out_file):
+    def simplify_file(self, in_file, out_file):
         Logger.log_message('Running Lexical Simplifier on ' + in_file)
 
         input_file = open(in_file, 'r')
@@ -68,3 +68,21 @@ class LexicalSimplifier:
         output_file.close()
 
         Logger.log_success('Results have been written to ' + out_file)
+
+    # Simplify a given content
+    @staticmethod
+    def simplify(content):
+        words = [str(word) for word in content.split()]
+        new_words = []
+
+        for word in words:
+            sanitized_word = Sanitizer.sanitize_word(word)
+
+            if sanitized_word == '':
+                continue
+
+            replacer = Replacer()
+            replaced_word = replacer.replacement(sanitized_word)
+            new_words.append(replaced_word)
+
+        return ' '.join(new_words) + '\m'
