1d8040adc35d48fe043d4b4525f34e8c89f69921
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2015-03-22 20:55:16 +0530

    Added an empty file for lwlm

---
+++ src/lexus/lwlm.py
@@ -0,0 +1,15 @@
+__author__ = 's7a'
+
+
+# The Latent Words Language Model
+class LWLM:
+
+    # Constructor for the LWLM
+    def __init__(self):
+        pass
+
+    # Get the lwlm words for a given word
+    @staticmethod
+    def get(word):
+        result = [word]
+        return result
