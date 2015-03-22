3f13b56819b7ab4055443db1b5d662f72d249cbd
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2015-03-22 23:13:53 +0530

    Added N Gram Frequnecy class - empty

---
+++ src/lexus/n_gram_frequency.py
@@ -0,0 +1,23 @@
+__author__ = 's7a'
+
+
+# Builds up the N-Gram Frequency table
+class NGramFrequency:
+
+    # Constructor for the N-Gram Frequency counter
+    def __init__(self, n, in_dir, out_file):
+        self.n = n
+        self.in_dir = in_dir
+        self.out_file = out_file
+
+    # Run the N-Gram Frequency Counter
+    def run(self):
+        pass
+
+    # Parse a given file and build the N-Gram frequency table
+    def parse_file(self, in_file):
+        pass
+
+    # Dump the results to the output file
+    def dump_results(self, out_file):
+        pass
