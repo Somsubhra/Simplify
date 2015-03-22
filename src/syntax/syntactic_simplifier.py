90e42d0da0ea67f23e42de0d6aa60e162168ef1e
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2015-03-22 21:23:07 +0530

    Added Syntactic simplifier skel

--- src/syntax/syntactic_simplifier.py
+++ src/syntax/syntactic_simplifier.py
@@ -2,6 +2,7 @@ __author__ = 's7a'
 
 # All imports
 from extras import Logger
+from os import walk, path, stat, makedirs
 
 
 # The Syntactic simplification class
@@ -15,4 +16,30 @@ class SyntacticSimplifier:
     # Run the Syntactic Simplifier
     def run(self):
         Logger.log_message('Running Syntactic Simplifier')
+
+        try:
+            stat(self.in_dir)
+        except:
+            Logger.log_error('Input text not found')
+            return
+
+        try:
+            stat(self.out_dir)
+        except:
+            makedirs(self.out_dir)
+
+        for(dir_path, _, file_names) in walk(self.in_dir):
+            for file_name in file_names:
+                in_file = path.join(dir_path, file_name)
+                out_file = path.join(self.out_dir, file_name + '_' + dir_path.replace('/', '_') + '.txt')
+
+                self.simplify_file(in_file, out_file)
+
         Logger.log_success('Syntactic Simplifier finished successfully')
+
+    def simplify_file(self, in_file, out_file):
+        pass
+
+    @staticmethod
+    def simplify(content):
+        pass
