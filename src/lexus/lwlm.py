b37669b33c1ee6842fe5edce0136e45961a778c0
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2015-03-23 11:44:21 +0530

    A function to get list of alternate words

--- src/lexus/lwlm.py
+++ src/lexus/lwlm.py
@@ -22,9 +22,34 @@ class LWLM:
         self.dump_results()
 
     # Get the lwlm words for a given word
-    def get(self, word):
-        result = [word]
-        return result
+    def get(self, word, neighbors, n):
+
+        # Construct the neighbor string
+        neighbor_str = ""
+
+        for i in range(n - 1):
+            neighbor_str += neighbors[i] + '|'
+
+            if i == n / 2 - 1:
+                neighbor_str += '|'
+
+        if n == 3:
+            neighbors_dict = self.three_gram_neighbors
+        elif n == 5:
+            neighbors_dict = self.five_gram_neighbors
+        else:
+            Logger.log_error('n = ' + str(n) + ' not supported in LWLM')
+            return []
+
+        if neighbor_str in neighbors_dict:
+            alt_words = list(neighbors_dict[neighbor_str])
+        else:
+            alt_words = []
+
+        if word not in alt_words:
+            alt_words.append(word)
+
+        return alt_words
 
     # Dump the results in an output file
     def dump_results(self):
