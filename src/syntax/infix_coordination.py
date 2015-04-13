2e7edc6809f5537ac45b9d940951f6af011a18f7
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2015-04-13 12:37:03 +0530

    Added new breaker classes

---
+++ src/syntax/infix_coordination.py
@@ -0,0 +1,27 @@
+__author__ = 's7a'
+
+# All imports
+from nltk.tree import Tree
+
+
+# The infix coordination class
+class InfixCoordination:
+
+    # Constructor for the infix coordination
+    def __init__(self):
+        self.has_infix_coordination = False
+
+    # Break the tree
+    def break_tree(self, tree):
+        t = Tree.fromstring(str(tree))
+
+        self.parse_tree(t)
+
+        result_string = ""
+        return result_string
+
+    # Parse the tree
+    def parse_tree(self, tree):
+        if type(tree) == Tree:
+            for node in tree:
+                self.parse_tree(node)
