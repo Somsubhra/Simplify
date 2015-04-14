7bc3e2bac42f57b939efca606f2d0fb3e9eb3cf7
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2015-04-14 23:30:47 +0530

    Break relative clause

--- src/syntax/relative_clauses.py
+++ src/syntax/relative_clauses.py
@@ -10,29 +10,70 @@ class RelativeClauses:
     # Constructor for the Relative Clauses class
     def __init__(self):
         self.has_wh_word = False
-        self.result_string = ""
+        self.np_subtrees = []
+        self.wh_subtrees = []
+        self.other_subtrees = []
 
     # Break the tree
     def break_tree(self, tree):
-        t = Tree.fromstring(str(tree))
-
         self.has_wh_word = False
-        self.result_string = ""
+        self.np_subtrees = []
+        self.wh_subtrees = []
+        self.other_subtrees = []
 
-        self.parse_tree(t)
+        self.parse_tree(tree)
 
         print "Relative Clause: " + str(self.has_wh_word)
 
-        return self.result_string
+        if self.has_wh_word:
+            wh_part = " ".join(" ".join(self.wh_subtrees).split()[1:])
+            result_string = " ".join(self.np_subtrees) + " " + wh_part + "."
+            result_string += (" ".join(self.np_subtrees) + " ".join(self.other_subtrees)).replace(",", "")
+        else:
+            result_string = " ".join(tree.leaves())
+
+        print "Relative Clause Result: " + result_string
+
+        return result_string
 
     # Parse the tree
     def parse_tree(self, tree):
-
         if type(tree) == Tree:
-            if tree.label() == "SBAR":
-                for node in tree:
-                    if type(node) == Tree:
-                        self.has_wh_word |= node.label() == "WHNP"
+            sentence_root = tree[0]
+            if type(sentence_root) == Tree:
+                if sentence_root.label() == "S":
+                    first_node = sentence_root[0]
+                    if type(first_node) == Tree:
+                        if first_node.label() == "NP":
+                            for node in first_node:
+                                if type(node) == Tree:
+                                    if node.label() == "NP":
+                                        self.np_subtrees.append(' '.join(node.leaves()))
+                                    elif node.label() == "SBAR":
+                                        node_1 = node[0]
+                                        if type(node_1) == Tree:
+                                            if node_1.label() == "WHNP":
+                                                self.has_wh_word |= True
+                                                self.wh_subtrees.append(' '.join(node.leaves()))
+                                            else:
+                                                self.other_subtrees.append(' '.join(node.leaves()))
+                                    else:
+                                        self.other_subtrees.append(' '.join(' '.join(node.leaves())))
+                                else:
+                                    self.other_subtrees.append(node)
+                        else:
+                            self.other_subtrees.append(' '.join(first_node.leaves()))
+                    else:
+                        self.other_subtrees.append(first_node)
 
-            for node in tree:
-                self.parse_tree(node)
+                    flag = 0
+
+                    for node in sentence_root:
+                        if flag == 0:
+                            flag = 1
+                            continue
+
+                        if type(node) == Tree:
+                            self.other_subtrees.append(' '.join(node.leaves()))
+                        else:
+                            self.other_subtrees.append(node)
