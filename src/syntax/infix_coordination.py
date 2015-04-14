a61a09a08322c3e74800c0635c4848280ad9341e
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2015-04-14 15:07:53 +0530

    Split the sentence in case of infix coordinatiin

--- src/syntax/infix_coordination.py
+++ src/syntax/infix_coordination.py
@@ -10,12 +10,27 @@ class InfixCoordination:
     # Constructor for the infix coordination
     def __init__(self):
         self.has_infix_coordination = False
+        self.slice_point = -1
+        self.subtree_list = []
 
     # Break the tree
     def break_tree(self, tree):
         self.has_infix_coordination = False
+        self.slice_point = -1
+        self.subtree_list = []
+
         self.parse_tree(tree)
+
         print "Infix Coordination: " + str(self.has_infix_coordination)
+        print self.slice_point
+        print self.subtree_list
+
+        result_string = ' '.join(self.subtree_list[:self.slice_point-1]) +\
+                        '. ' + ' '.join(self.subtree_list[self.slice_point:])
+
+        print result_string
+        
+        return result_string
 
     # Parse the tree
     def parse_tree(self, tree):
@@ -24,7 +39,14 @@ class InfixCoordination:
             if type(sentence_root) == Tree:
                 if sentence_root.label() == "S":
                     print "Valid Tree"
+
+                    counter = 0
                     for node in sentence_root:
+                        counter += 1
+
+                        self.subtree_list.append(' '.join(node.leaves()))
+
                         if type(node) == Tree:
                             if node.label() == "CC":
                                 self.has_infix_coordination |= True
+                                self.slice_point = counter
