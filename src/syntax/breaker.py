1058754638f5f423a328a3f0e12b5bca3b394256
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2015-04-22 21:13:22 +0530

    Give a cumulative result for syntax

--- src/syntax/breaker.py
+++ src/syntax/breaker.py
@@ -1,11 +1,14 @@
 __author__ = 's7a'
 
 # All imports
+from parser import Parser
 from appositions import Appositions
 from relative_clauses import RelativeClauses
 from prefix_subordination import PrefixSubordination
 from infix_subordination import InfixSubordination
 from infix_coordination import InfixCoordination
+
+import re
 
 
 # Constructor for the breaker class
@@ -13,6 +16,7 @@ class Breaker:
 
     # Constructor for the breaker class
     def __init__(self):
+        self.parser = Parser()
         self.appositions = Appositions()
         self.relative_clauses = RelativeClauses()
         self.prefix_subordination = PrefixSubordination()
@@ -27,11 +31,37 @@ class Breaker:
         infix_subordination_result = self.infix_subordination.break_tree(tree)
         infix_coordination_result = self.infix_coordination.break_tree(tree)
 
+        res_1 = self.appositions.break_tree(tree)
+        res_2 = self.break_tree_with_type(res_1, self.relative_clauses)
+        res_3 = self.break_tree_with_type(res_2, self.prefix_subordination)
+        res_4 = self.break_tree_with_type(res_3, self.infix_subordination)
+        res_5 = self.break_tree_with_type(res_4, self.infix_coordination)
+
         return {
             "original": ' '.join(tree.leaves()),
+            "final": res_5,
             "apposition": apposition_result,
             "relative_clause": relative_clause_result,
             'prefix_subordination': prefix_subordination_result,
             "infix_subordination": infix_subordination_result,
             "infix_coordination": infix_coordination_result
         }
+
+    def break_tree_with_type(self, string, breaker_type):
+        sentences = re.split('\.|!|\?', string)
+
+        result = ""
+
+        for sentence in sentences:
+            sentence += "."
+
+            if sentence == ".":
+                continue
+
+            parse_trees = self.parser.parse(sentence, False)
+
+            for parse_tree in parse_trees:
+                broken_string = breaker_type.break_tree(parse_tree)
+                result += broken_string
+
+        return result
