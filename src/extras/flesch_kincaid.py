b77289ddf8a065a64fdf003e50cedc4b62dd1e80
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2015-04-23 11:15:30 +0530

    Fix Flesch Kincaid grade level + Add some console log

--- src/extras/flesch_kincaid.py
+++ src/extras/flesch_kincaid.py
@@ -32,12 +32,23 @@ class FleschKincaid:
                 number_of_syllables += syllables_en.count_syllables(sanitized_word)
                 words.append(sanitized_word)
 
-        sentences = re.split('\.|!|\?', content)
+        token_arrays = re.split('\.|!|\?', content)
+        sentences = []
+
+        for sentence in token_arrays:
+            if sentence not in garbage_words:
+                sentences.append(sentence)
 
         number_of_sentences = len(sentences)
         number_of_words = len(words)
 
-        grade_level = 0.39 * (number_of_words / number_of_sentences) +\
-                      11.8 * (number_of_syllables / number_of_words) - 15.59
+        print "Syllables: " + str(number_of_syllables)
+        print "Words: " + str(number_of_words)
+        print str(words)
+        print "Sentences: " + str(number_of_sentences)
+        print str(sentences)
+
+        grade_level = 0.39 * (float(number_of_words) / float(number_of_sentences)) +\
+                      11.8 * (float(number_of_syllables) / float(number_of_words)) - 15.59
 
         return grade_level
